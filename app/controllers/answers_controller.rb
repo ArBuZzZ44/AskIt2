2ae5dd9e03738773be1f481f9805779eabc87cd2
ArBuZzZ44 <zahar1702@mail.ru>
2023-02-20 02:47:37 +0700

    transferring files from the first application

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,2 +1,50 @@
 class AnswersController < ApplicationController
+  include ActionView::RecordIdentifier
+
+  before_action :set_question! # порядок этих before важен, т.к. сначала нужно найти вопрос, а потом на основе вопроса ответ
+  before_action :set_answer!, except: :create
+
+  def create
+    @answer = @question.answers.build answer_params
+
+    if @answer.save
+      flash[:success] = "Answer created!"
+      redirect_to question_path(@question)
+    else
+       @pagy, @answers = pagy @question.answers.order(created_at: :desc)
+      render 'questions/show'
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @answer.update answer_params
+      flash[:success] = "Answer updated!"
+      redirect_to question_path(@question, anchor: dom_id(@answer)) # при обновлении ответа показывает нам обновленный ответ
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+   @answer.destroy
+   flash[:success] = "Answer deleted!"
+   redirect_to question_path(@question)
+  end
+
+  private
+
+  def set_question!
+    @question = Question.find params[:question_id]
+  end
+
+  def answer_params
+    params.require(:answer).permit(:body)
+  end
+
+  def set_answer!
+    @answer = @question.answers.find params[:id]
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resource :session, only: %i[new create destroy] # для входа в пользователя в систему, destroy, чтобы пользователь мог выходить
 
-  # Defines the root path route ("/")
-   root "pages#index"
+  resources :users, only: %i[new create]
+
+  resources :questions do 
+    resources :answers, except: %i[new show] # except - кроме, т.е. исключаем ненужные маршруты
+  end
+
+  root "pages#index"
 end
