b1402e54202d9649f33766f1d3f4c5c70e2d59e3
ArBuZzZ44 <zahar1702@mail.ru>
2023-02-22 01:05:48 +0700

    minor fix

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,5 @@
 class SessionsController < ApplicationController
-  before_action :require_no_authentication only: %i[new create]
+  before_action :require_no_authentication, only: %i[new create]
   before_action :require_authentication, only: :destroy
 
   def new
