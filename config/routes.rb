6088bf0c900940492beb2441c13e1e01afec40cc
ArBuZzZ44 <zahar1702@mail.ru>
2023-02-22 12:37:57 +0700

    add routes for edit profile

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resource :session, only: %i[new create destroy] # для входа в пользователя в систему, destroy, чтобы пользователь мог выходить
 
-  resources :users, only: %i[new create]
+  resources :users, only: %i[new create edit update]
 
   resources :questions do 
     resources :answers, except: %i[new show] # except - кроме, т.е. исключаем ненужные маршруты
