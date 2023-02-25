c133ebf8411d9dfca95cdfd2a7c6af4db7dfa2d9
ArBuZzZ44 <zahar1702@mail.ru>
2023-02-26 06:08:24 +0700

    minor fix

--- app/services/application_service.rb
+++ app/services/application_service.rb
@@ -1,5 +1,5 @@
 class ApplicationService
   def self.call(*args, &block)
-    new.(*args, &block).call # инстанцирует указанный класс и вызывает метод
+    new(*args, &block).call # инстанцирует указанный класс и вызывает метод
   end
 end
