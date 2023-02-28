abb76b1243cd79e08a9e89afddc886f34e59a2f4
ArBuZzZ44 <zahar1702@mail.ru>
2023-02-28 15:12:38 +0700

    creating a route for switching locale

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,23 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  # для входа в пользователя в систему, destroy, чтобы пользователь мог выходить
-  resource :session, only: %i[new create destroy]
+  # скобки чтобы можно было в маршрут передавать локаль либо не передавать
+  # локали передаем из конфига с помощью регулярного выражения 
+  scope "(:locale)", locale: /#{I18n.available_locales.join("|")}/ do
+    # для входа в пользователя в систему, destroy, чтобы пользователь мог выходить
+    resource :session, only: %i[new create destroy]
 
-  resources :users, only: %i[new create edit update]
+    resources :users, only: %i[new create edit update]
 
-  resources :questions do
-    resources :answers, except: %i[new show] # except - кроме, т.е. исключаем ненужные маршруты
+    resources :questions do
+      resources :answers, except: %i[new show] # except - кроме, т.е. исключаем ненужные маршруты
+    end
+
+    # создаем пространство имен для admin и маршрут для этого имени
+    namespace :admin do
+      resources :users, only: %i[index create]
+    end
+
+    root 'pages#index'
   end
-
-  # создаем пространство имен для admin и маршрут для этого имени
-  namespace :admin do
-    resources :users, only: %i[index create]
-  end
-
-  root 'pages#index'
 end
