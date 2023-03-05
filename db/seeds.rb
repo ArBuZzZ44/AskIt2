ec36ceb24ae4f4ff9e781d26bfc5f3f47858a9ba
ArBuZzZ44 <zahar1702@mail.ru>
2023-03-06 01:15:43 +0700

    adding gravatar_hash for existing users

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,8 +7,11 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-30.times do
-  body = Faker::Lorem.paragraph(sentence_count: 5, supplemental: true, random_sentences_to_add: 4)
-  title = Faker::Hipster.sentence(word_count: 3)
-  Question.create title:, body:
-end
+# 30.times do
+#   body = Faker::Lorem.paragraph(sentence_count: 5, supplemental: true, random_sentences_to_add: 4)
+#   title = Faker::Hipster.sentence(word_count: 3)
+#   Question.create title:, body:
+# end
+
+# с помощью send можно вызывать приватные методы
+User.find_each {|u| u.send(:set_gravatar_hash) ; u.save}
