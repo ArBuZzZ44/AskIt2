67f7fe895a6186ee80f3670a6f451bca876c9a06
ArBuZzZ44 <zahar1702@mail.ru>
2023-03-06 23:31:30 +0700

    a new concern that we will connect to other models to create comments

---
+++ app/models/concerns/commentable.rb
@@ -0,0 +1,9 @@
+module Commentable
+  extend ActiveSupport::Concern
+
+  included do
+    # к любой модели можем подключить этот concern и у этой модели будет много комментариев
+    # и эти виртуальные отношения будут называться commentable
+    has_many :comments, as: :commentable, dependent: :destroy
+  end
+end
