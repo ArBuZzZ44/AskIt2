42d5315f5d86ad82362f2725100960a958575dca
ArBuZzZ44 <zahar1702@mail.ru>
2023-03-06 23:31:58 +0700

    connecting concern with comments

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,9 +1,11 @@
 # frozen_string_literal: true
 
 class Question < ApplicationRecord
+  include Commentable
+
   has_many :answers, dependent: :destroy # если удалить question, то все зависимые ответы тоже долнжы удалиться
   belongs_to :user
-  
+
   validates :title, presence: true, length: { minimum: 2 }
   validates :body, presence: true, length: { minimum: 2 }
 end
