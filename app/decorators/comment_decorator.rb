5459a89d52a0ad6327017a1957e42467271f9d89
ArBuZzZ44 <zahar1702@mail.ru>
2023-03-09 18:50:01 +0700

    we determine what the comment is left for

--- app/decorators/comment_decorator.rb
+++ app/decorators/comment_decorator.rb
@@ -1,4 +1,11 @@
 class CommentDecorator < ApplicationDecorator
   delegate_all
   decorates_association :user
+
+  # определяем, для вопроса или ответа комментарий
+  def for?(commentable)
+    # если commentable задекорирован, то берем оттуда конкретный образер класса
+    commentable = commentable.object if commentable.decorated?
+    commentable == self.commentable
+  end
 end
