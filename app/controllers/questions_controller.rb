990531a4a7f181673cce2511987d8ed7ee7a0ebf
ArBuZzZ44 <zahar1702@mail.ru>
2023-03-09 23:07:24 +0700

    improve index method

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -6,7 +6,7 @@ class QuestionsController < ApplicationController
 
   def index
     # метод pagy возвращает массив из двух элементов. передаем объект, который хотим разбить по страницам
-    @pagy, @questions = pagy Question.order(created_at: :desc)
+    @pagy, @questions = pagy Question.includes([:user]).order(created_at: :desc)
     @questions = @questions.decorate
   end
 
