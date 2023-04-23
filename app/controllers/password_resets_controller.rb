cd4bc16a5c4d6aa194d7a142f8cf0eeb749cc35c
ArBuZzZ44 <zahar1702@mail.ru>
2023-04-24 00:17:20 +0700

    controller for reset password

---
+++ app/controllers/password_resets_controller.rb
@@ -0,0 +1,8 @@
+class PasswordResetsController < ApplicationController
+  before_action :require_no_authentication
+
+
+  def create
+    PasswordResetMailer.with(user: @user).reset_email.deliver_later
+  end
+end
