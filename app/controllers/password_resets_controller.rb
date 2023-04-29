32808e98e0727a0f08d21fb131d9e454b6b71ddd
ArBuZzZ44 <zahar1702@mail.ru>
2023-04-30 03:38:10 +0700

    add method edit

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,5 +1,6 @@
 class PasswordResetsController < ApplicationController
   before_action :require_no_authentication
+  before_action :set_user, only: %i[edit update]
 
 
   def create
@@ -13,5 +14,18 @@ class PasswordResetsController < ApplicationController
 
     flash[:success] = 'The email has been sent'
     redirect_to new_session_path
+  end
+
+  def edit
+
+  end
+
+  private
+
+  def set_user
+    @user = User.find_by email: params[:user][:email],
+                         password_reset_token: params[:user][:password_reset_token]
+
+    redirect_to(new_session_path, flash: { warning: 'Could not reset your password'}) unless @user&.password_reset_period_valid?
   end
 end
