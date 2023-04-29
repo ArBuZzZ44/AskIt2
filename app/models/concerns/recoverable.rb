a84acef39af14c47e52aa6f90b1e95a9432b8521
ArBuZzZ44 <zahar1702@mail.ru>
2023-04-30 03:38:29 +0700

    add validation for reset token

--- app/models/concerns/recoverable.rb
+++ app/models/concerns/recoverable.rb
@@ -6,5 +6,10 @@ module Recoverable
       update password_reset_token: digest(SecureRandom.urlsafe_base64),
               password_reset_token_sent_at: Time.current
     end
+
+    def password_reset_period_valid?
+      # если разница больше, чем 60 минут, значит токен истек
+      password_reset_token_sent_at.present? && Time.current - password_reset_token_sent_at <= 60.minutes
+    end
   end
 end
