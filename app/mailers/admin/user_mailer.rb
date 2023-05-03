bd569793f18e2bcdf8e3c62c0d9a35e0ccd09302
ArBuZzZ44 <zahar1702@mail.ru>
2023-05-04 00:47:39 +0700

    methods for sending emails

---
+++ app/mailers/admin/user_mailer.rb
@@ -0,0 +1,24 @@
+module Admin
+  class UserMailer < ApplicationMailer
+    def bulk_import_done 
+      @user = params[:user]
+
+      mail to: @user.email, subject: I18n.t('admin.user_mailer.bulk_import_done.subject')
+    end
+
+    def bulk_import_fail 
+      @error = params[:error]
+      @user = params[:user]
+
+      mail to: @user.email, subject: I18n.t('admin.user_mailer.bulk_import_fail.subject')
+    end
+
+    def bulk_export_done 
+      @user = params[:user]
+      zipped_blob = params[:zipped_blob]
+
+      attachments[zipped_blob.attachable_filename] = zipped_blob.download # для пристыковки файла к письму
+      mail to: @user.email, subject: I18n.t('admin.user_mailer.bulk_export_done.subject')
+    end
+  end
+end
