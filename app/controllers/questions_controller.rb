d5874fcdf155eb7bb14104dfc14dc8f8a8d19746
ArBuZzZ44 <zahar1702@mail.ru>
2023-05-07 06:05:05 +0700

    fix delete method

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -32,7 +32,7 @@ class QuestionsController < ApplicationController
       flash[:success] = 'Question created!'
       redirect_to questions_path
     else
-      render :new
+      render :new, status: :unprocessable_entity
     end
   end
 
@@ -41,7 +41,7 @@ class QuestionsController < ApplicationController
       flash[:success] = 'Question updated!'
       redirect_to questions_path
     else
-      render :edit
+      render :edit, status: :unprocessable_entity
     end
   end
 
